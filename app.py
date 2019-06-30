2fe476eb90f9899f64cee46508d5ecea5b800a9c
Jaskaran <jaskarannagi19@gmail.com>
2019-07-01 00:18:44 +0530

    Final

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 # Importing flask module in the project is mandatory 
 # An object of Flask class is our WSGI application. 
 from flask import Flask, render_template,request,jsonify,make_response
-from flask_cors import CORS
 from watson import  chatbot
 import json
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument. 
 app = Flask(__name__) 
-CORS(app)
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function. 
@@ -21,18 +19,19 @@ def hello_world():
 @app.route('/app', methods=['POST'])
 def hello():
 	#print("______________________")
-	#data = request.json['query']
+	query = request.form['query']
 	#print(data)
 	#print("______________________")
-	query= str(request.data)
-	
+	#query= str(request.data)
+	#print(query)
 	result = c.submit(query)
-	resp = make_response( result['b'] )
+	resp = make_response(result['b'])
 	resp.headers['Access-Control-Allow-Origin'] = '*'
+	resp.mimetype = "application/javascript"
 	return resp
 # main driver function 
 if __name__ == '__main__': 
 	c = chatbot()
 	# run() method of Flask class runs the application 
 	# on the local development server. 
-	app.run() 
+	app.run(debug='TRUE') 
