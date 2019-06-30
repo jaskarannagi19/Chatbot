b96cb25282427e06e16bbdaeaea6a5e88cc6e690
Jaskaran <jaskarannagi19@gmail.com>
2019-06-30 23:26:24 +0530

    CORS

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # Importing flask module in the project is mandatory 
 # An object of Flask class is our WSGI application. 
 from flask import Flask, render_template,request,jsonify,make_response
+from flask_cors import CORS
 from watson import  chatbot
 import json
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument. 
 app = Flask(__name__) 
-
+CORS(app)
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function. 
